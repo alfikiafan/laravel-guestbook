94b1556d68996bf8a5a8e9464578c2a1a2c1d378
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-06-01 11:11:45 +0700

    Change to English

--- resources/views/guestbook/result.blade.php
+++ resources/views/guestbook/result.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
     <div class="container mt-5">
-        <h2>Result</h2>
-        <a href="{{ route('home') }}" class="btn btn-primary">Kembali ke Halaman Home</a>
-        <a href="{{ route('guestbook.view') }}" class="btn btn-secondary">Lihat Buku Tamu</a>
-        <h5 class="card-title">Hasil dari Post:</h5>
+        <h2>POST Result</h2>
+        <a href="{{ route('home') }}" class="btn btn-primary">Back to Home</a>
+        <a href="{{ route('guestbook.view') }}" class="btn btn-secondary">View All Guestbook</a>
         <div class="card">
             <div class="card-body">
                 <pre>{{ var_export($guestbookData, true) }}</pre>
